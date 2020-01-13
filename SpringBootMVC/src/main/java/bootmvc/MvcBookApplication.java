c68c9b2f6ca938b77367f5719d0b5af20f671699
ashish001rathod <59167059+ashish001rathod@users.noreply.github.com>
2020-01-13 17:17:39 +0530

    Add files via upload

---
+++ SpringBootMVC/src/main/java/bootmvc/MvcBookApplication.java
@@ -0,0 +1,14 @@
+package bootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MvcBookApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MvcBookApplication.class,args);
+
+	}
+
+}
